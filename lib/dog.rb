81e3e19bde355a3b3410bb3d360c6f94d5d37fbd
Lon Bitner <lonbitner@gmail.com>
2020-04-27 20:34:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   def name=(dog_name)
     this_dogs_name =
 dog_name
-end
+  end
 
   def name
     this_dogs_name
@@ -10,7 +10,3 @@ end
 end
 
 
-lassie = Dog.new
-lassie.name = "Lassie"
-
-lassie.name
